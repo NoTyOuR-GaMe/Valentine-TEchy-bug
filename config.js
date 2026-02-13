e064a75cff43e808db141edbc0fa359a25ea9827
NoTyOuR-GaMe <krevhenry@gmail.com>
2026-02-13 17:36:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dnscnydme/video/upload/v1770980253/Like_Real_People_Do_spotdown.org_zjczjv.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dnscnydme/video/upload/v1770984282/SUPERMAN_neq8vs.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.9                        // Volume level (0.0 to 1.0)
