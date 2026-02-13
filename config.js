eb617cd8d834981451ced2f3369d5db631125784
NoTyOuR-GaMe <krevhenry@gmail.com>
2026-02-13 16:30:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -79,10 +79,10 @@ that ignores gravity",              // Shows when they go past 1000%
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dnscnydme/video/upload/v1770980253/Like_Real_People_Do_spotdown.org_zjczjv.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 0.9                        // Volume level (0.0 to 1.0)
     }
 };
 
